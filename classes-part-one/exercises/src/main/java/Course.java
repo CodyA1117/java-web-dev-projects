b4273f40df215b0355708d1addc026e0cd01ff09
CodyA1117 <adamscody079@gmail.com>
2024-09-12 14:21:52 -0500

    Updated exercise for classes and objects.

---
+++ classes-part-one/exercises/src/main/java/Course.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+
+public class Course {
+    private String topic;
+    private Teacher instructor;
+    private ArrayList<Student> enrolledStudents;
+
+    // Constructor
+    public Course(String topic, Teacher instructor) {
+        this.topic = topic;
+        this.instructor = instructor;
+        this.enrolledStudents = new ArrayList<>();
+    }
+
+    // Getters and Setters
+    public String getTopic() {
+        return topic;
+    }
+
+    public void setTopic(String topic) {
+        this.topic = topic;
+    }
+
+    public Teacher getInstructor() {
+        return instructor;
+    }
+
+    public void setInstructor(Teacher instructor) {
+        this.instructor = instructor;
+    }
+
+    public ArrayList<Student> getEnrolledStudents() {
+        return enrolledStudents;
+    }
+
+    public void addStudent(Student student) {
+        enrolledStudents.add(student);
+    }
+}
--- classes-part-one/exercises/src/main/java/StudentPractice.java
+++ classes-part-one/exercises/src/main/java/StudentPractice.java
@@ -1,5 +1,11 @@
 public class StudentPractice {
     public static void main(String[] args){
         //insantiate your Student class below
+        Student student = new Student("Cody Adams", 12345, 1, 4.0);
+
+        System.out.println("Student Name: " + student.getName());
+        System.out.println("Student ID: " + student.getStudentId());
+        System.out.println("Number of Credits: " + student.getNumberOfCredits());
+        System.out.println("GPA: " + student.getGpa());
     }
 }
