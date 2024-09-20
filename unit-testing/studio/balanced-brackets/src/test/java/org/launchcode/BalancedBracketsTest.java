564aac478301e6f54429828b93aa3bb2a0406bf1
CodyA1117 <adamscody079@gmail.com>
2024-09-19 20:43:25 -0500

    Completed Studio

--- unit-testing/studio/balanced-brackets/src/test/java/org/launchcode/BalancedBracketsTest.java
+++ unit-testing/studio/balanced-brackets/src/test/java/org/launchcode/BalancedBracketsTest.java
@@ -10,4 +10,56 @@ class BalancedBracketsTest {
     public void emptyTest() {
         assertEquals(true, true);
     }
+
+    @Test
+    public void onlyBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
+    }
+
+    @Test
+    public void singBracketsAroundOtherCharachters(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[LaunchCode]"));
+    }
+    @Test
+    public void launchBracketCodeBracket(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("Launch[Code]"));
+    }
+    @Test
+    public void openBracketClosingBracketCharachter(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]LaunchCode"));
+    }
+    @Test
+    public void openOpenCloseClose(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[[]]"));
+    }
+    @Test
+    public void openCharacter(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[LaunchCode"));
+    }
+    @Test
+    public void closeClose(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]]"));
+    }
+    @Test
+    public void openOpen(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[["));
+    }
+    @Test
+    public void openCloseThreeOpenThreeCloseThree(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[][][][[[]]]"));
+    }
+    @Test
+    public void charClosingCharOpen(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("Launch]Code["));
+    }
+    @Test
+    public void StringNoBrackets(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("LaunchCode"));
+    }
+
+
+
+
+
+
 }
