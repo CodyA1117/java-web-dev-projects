9563e15890dcef5fecdd433beab6a0a4b758f3e8
CodyA1117 <adamscody079@gmail.com>
2025-02-14 06:23:44 -0600

    completed the exercise on interfaces.

--- interfaces/exercises/ice-cream-exercises/src/main/java/org/launchcode/Main.java
+++ interfaces/exercises/ice-cream-exercises/src/main/java/org/launchcode/Main.java
@@ -1,12 +1,46 @@
 package org.launchcode;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 
 public class Main {
     public static void main(String[] args) {
         Case menu = new Case();
         ArrayList<Flavor> flavors = menu.getFlavors();
         ArrayList<Cone> cones = menu.getCones();
+        Comparator comparator = new FlavorComparator();
+        Comparator comparator1 = new ConeComparator();
+        System.out.println("Before:");
+        System.out.println();
+        for (Flavor flavor : flavors) {
+            System.out.println(flavor.getName());
+        }
+
+        flavors.sort(comparator);
+
+        System.out.println();
+        System.out.println("After:");
+        System.out.println();
+        for (Flavor flavor : flavors) {
+            System.out.println(flavor.getName());
+        }
+
+        System.out.println("Before:");
+        System.out.println();
+        for (Cone cone : cones) {
+            System.out.println(cone.getName());
+        }
+
+        cones.sort(comparator1);
+
+        System.out.println();
+        System.out.println("After:");
+        System.out.println();
+        for (Cone cone : cones) {
+            System.out.println(cone.getName());
+        }
+
+
 
         // TODO: Use a Comparator class to sort the 'flavors' array alphabetically by the 'name' field.
 
